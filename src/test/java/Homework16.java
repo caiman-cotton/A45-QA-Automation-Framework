0b05b583794ce26aa93632d58e66dbaf3dc3c640
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-04-20 22:28:25 -0500

    Created registrationNavigation test

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework16 extends BaseTest {
+    @Test
+    public static void registrationNavigation() {
+        //ChromeOptions arguments
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //set and get URL
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        //click registration
+        WebElement registration = driver.findElement(By.cssSelector("a[type='submit']"));
+        registration.click();
+        //unique element to verify reg page
+        WebElement uniqueForm = driver.findElement(By.cssSelector("form[method='POST']"));
+        //verify reg page
+        Assert.assertTrue(uniqueForm.isDisplayed());
+
+        driver.quit();
+    }
+}
