5af2fffe62027662d40f9c1e1cbb3764bb66e1b3
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-04-20 23:49:01 -0500

    Created registrationNavigation test

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.time.Duration;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 
 public class Homework16 extends BaseTest {
     @Test
