f9903e7b67069a51b9177567e61683432509a313
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-04-27 13:26:37 -0500

    Added BaseTest methods

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,54 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
 
 public class BaseTest {
+    public static WebDriver driver = null;
+    public static String link = "https://bbb.testpro.io";
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
+    @BeforeMethod
+    public void launchChrome() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void endTest() {
+        driver.quit();
+    }
+    public static void goToPage() {
+        driver.get(link);
+    }
+    public static void logInEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+    public static void inputPassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+    public static void clickSubmit() {
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitBtn.click();
+    }
+
 }
---
+++ src/test/java/Homework17Test.java
@@ -0,0 +1,9 @@
+public class Homework17Test extends BaseTest{
+    @Test
+    public void addSongToPlaylist() {
+        String newSongAddedNotificationText = "Added 1 song into";
+
+
+        }
+    }
+}
