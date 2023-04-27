25dc8fe7c367500f1d6b190a8b50d16ad9c3f4d8
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-04-27 18:24:56 -0500

    Added Homework17Test

--- src/test/java/Homework16Test.java
+++ src/test/java/Homework16Test.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 
 public class Homework16Test extends BaseTest{
     @Test
-    public static void registrationNavigation() {
+    public  void registrationNavigation() {
         //stuff to make the test work in Chrome
      launchChrome();
 
