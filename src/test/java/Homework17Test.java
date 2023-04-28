f8ea42e674ebd03f582ef24a310332cd675d495f
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-04-27 19:13:34 -0500

    Added Homework17Test

--- src/test/java/Homework17Test.java
+++ src/test/java/Homework17Test.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class Homework17Test extends BaseTest{
     @Test
     public void addSongToPlaylist() throws InterruptedException {
-        String newSongAddedNotificationText = "Added 1 song into";
+        String newSongAddedNotificationText = "Added 1 song into \"playlist\"";
 launchChrome();
 goToPage();
 inputEmail("caiman.cotton@testpro.io");
@@ -14,7 +14,7 @@ clickViewAllBtn();
 selectFirstSongBySearch();
 clickAddToBtn();
 addToPlaylist("playlist");
-Assert.assertTrue(getNotificationText().contains(newSongAddedNotificationText + "playlist"));
+Assert.assertTrue(getNotificationText().contains(newSongAddedNotificationText));
 endTest();
 
         }
