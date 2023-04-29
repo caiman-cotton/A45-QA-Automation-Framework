4c019244b59dc7f6df763cc56e959ae13912963f
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-04-29 10:44:38 -0500

    Changed Homework18Test

--- src/test/java/Homework18Test.java
+++ src/test/java/Homework18Test.java
@@ -3,14 +3,12 @@ import org.testng.annotations.Test;
 
 public class Homework18Test extends BaseTest{
     @Test
-    public void playSong() throws InterruptedException {
-        launchChrome();
+    public void playSong() {
         goToPage();
         inputEmail("caiman.cotton@testpro.io");
         inputPassword("te$t$tudent");
         clickSubmit();
         playASong();
         Assert.assertTrue(isSongPlaying());
-        endTest();
     }
 }
