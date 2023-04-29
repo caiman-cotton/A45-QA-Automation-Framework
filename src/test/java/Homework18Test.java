93e78fb7ecf6efc3ce73b2b0067a1e7005eee566
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-04-29 11:36:51 -0500

    Homework20

--- src/test/java/Homework18Test.java
+++ src/test/java/Homework18Test.java
@@ -4,7 +4,6 @@ import org.testng.annotations.Test;
 public class Homework18Test extends BaseTest{
     @Test
     public void playSong() {
-        goToPage();
         inputEmail("caiman.cotton@testpro.io");
         inputPassword("te$t$tudent");
         clickSubmit();
