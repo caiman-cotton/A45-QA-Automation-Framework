4d15c51e645c9c8c85236a68bb17d52097a144c1
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-05-02 16:29:09 -0500

    Homework21

--- src/test/java/Homework21Test.java
+++ src/test/java/Homework21Test.java
@@ -4,6 +4,7 @@ public class Homework21Test extends BaseTest{
     @Test
     public void renamePlaylist() {
         String renamedNotificationText = "Updated playlist \"renamedPlaylist.\"";
+        logInMeToKoel();
         //create playlist so we can change its name
         createPlaylist("defaultName");
         clickDoublePlaylist("defaultName");
