d49ca9647bc162a25997e58e6e33ebac891d2de1
caiman-cotton <131096241+caiman-cotton@users.noreply.github.com>
2023-05-11 13:42:02 -0500

    Homework 25

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -26,6 +27,7 @@ public class BaseTest {
     public static String link;
     WebDriverWait wait;
     Actions actions;
+    ThreadLocal<WebDriver> threadDriver;
 
     @BeforeSuite
     static void setupClass() {
@@ -36,10 +38,15 @@ public class BaseTest {
     @Parameters({"BaseURL"})
     public void launchBrowser(String BaseURL) {
         link = BaseURL;
+        threadDriver = new ThreadLocal<>();
         driver = pickBrowser(System.getProperty("browser"));
-        driver.get(link);
+        threadDriver.set(driver);
+        getDriver().get(link);
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         actions = new Actions(driver);
+    }
+    public WebDriver getDriver() {
+        return threadDriver.get();
     }
     public static WebDriver pickBrowser(String browser) {
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -47,7 +54,9 @@ public class BaseTest {
         switch (browser) {
             case "MicrosoftEdge" -> {
                 WebDriverManager.edgedriver().setup();
-                return driver = new EdgeDriver();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new EdgeDriver(edgeOptions);
             }
             case "grid-edge" -> {
                 caps.setCapability("browserName", "MicrosoftEdge");
@@ -95,76 +104,77 @@ public class BaseTest {
 }
     @AfterMethod
     public void endTest() {
-        driver.quit();
+        getDriver().quit();
+        threadDriver.remove();
     }
 
-    public static void inputEmail(String email) {
-        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    public void inputEmail(String email) {
+        WebElement emailField = getDriver().findElement(By.cssSelector("input[type='email']"));
         emailField.click();
         emailField.clear();
         emailField.sendKeys(email);
     }
-    public static void inputPassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+    public void inputPassword(String password) {
+        WebElement passwordField = getDriver().findElement(By.cssSelector("input[type='password']"));
         passwordField.click();
         passwordField.clear();
         passwordField.sendKeys(password);
     }
     public void clickSubmit() {
-        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+        WebElement submitBtn = getDriver().findElement(By.cssSelector("button[type='submit']"));
         submitBtn.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("i[data-testid='sidebar-create-playlist-btn']")));
     }
 public void searchSongTitle(String songTitle) {
-        WebElement searchSong = driver.findElement(By.cssSelector("input[type='search']"));
+        WebElement searchSong = getDriver().findElement(By.cssSelector("input[type='search']"));
         searchSong.click();
                 searchSong.sendKeys(songTitle);
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.results section.songs h1 button")));
 }
 public void clickViewAllBtn() {
-        WebElement viewAllBtn = driver.findElement(By.cssSelector("div.results section.songs h1 button"));
+        WebElement viewAllBtn = getDriver().findElement(By.cssSelector("div.results section.songs h1 button"));
         viewAllBtn.click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("section#songResultsWrapper tr.song-item td.title")));
 }
 public void selectFirstSongBySearch() {
-        WebElement firstSongResult = driver.findElement(By.cssSelector("section#songResultsWrapper tr.song-item td.title"));
+        WebElement firstSongResult = getDriver().findElement(By.cssSelector("section#songResultsWrapper tr.song-item td.title"));
         firstSongResult.click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button.btn-add-to")));
 }
 public void clickAddToBtn() {
-        WebElement addToBtn = driver.findElement(By.cssSelector("button.btn-add-to"));
+        WebElement addToBtn = getDriver().findElement(By.cssSelector("button.btn-add-to"));
         addToBtn.click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(), 'playlist')]")));
 }
 public void addToPlaylist() {
-        WebElement ourPlaylist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(), 'playlist')]"));
+        WebElement ourPlaylist = getDriver().findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(), 'playlist')]"));
         ourPlaylist.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
 }
 public String getNotificationText() {
-        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        WebElement notification = getDriver().findElement(By.cssSelector("div.success.show"));
         return notification.getText();
 }
     public void playASong() {
-        WebElement playNextBtn = driver.findElement(By.cssSelector("i[data-testid='play-next-btn']"));
+        WebElement playNextBtn = getDriver().findElement(By.cssSelector("i[data-testid='play-next-btn']"));
 
-        WebElement playBtn = driver.findElement(By.cssSelector("span[data-testid='play-btn']"));
+        WebElement playBtn = getDriver().findElement(By.cssSelector("span[data-testid='play-btn']"));
         playNextBtn.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[data-testid='play-btn']")));
         playBtn.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[data-testid='sound-bar-play']")));
     }
     public boolean isSongPlaying() {
-        WebElement soundImage = driver.findElement(By.cssSelector("div[data-testid='sound-bar-play']"));
+        WebElement soundImage = getDriver().findElement(By.cssSelector("div[data-testid='sound-bar-play']"));
         return soundImage.isDisplayed();
     }
     public void createPlaylist(String nameOfPlaylist) {
-        WebElement playlistCreationBtn = driver.findElement(By.cssSelector("i[data-testid='sidebar-create-playlist-btn']"));
+        WebElement playlistCreationBtn = getDriver().findElement(By.cssSelector("i[data-testid='sidebar-create-playlist-btn']"));
         playlistCreationBtn.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']")));
-        WebElement newPlaylistCreation = driver.findElement(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']"));
+        WebElement newPlaylistCreation = getDriver().findElement(By.cssSelector("li[data-testid='playlist-context-menu-create-simple']"));
         newPlaylistCreation.click();
-        WebElement fieldNamePlaylist = driver.findElement(By.xpath("//section[@id='playlists']//input[@name='name']"));
+        WebElement fieldNamePlaylist = getDriver().findElement(By.xpath("//section[@id='playlists']//input[@name='name']"));
         fieldNamePlaylist.click();
         fieldNamePlaylist.sendKeys(nameOfPlaylist);
         fieldNamePlaylist.sendKeys(Keys.ENTER);
@@ -172,28 +182,28 @@ public String getNotificationText() {
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.success.show")));
     }
     public void clickPlaylistName(String nameOfPlaylist) {
-        WebElement myPlaylist = driver.findElement(By.xpath("//section[@id='playlists']//a[contains(text(), '" + nameOfPlaylist + "')]"));
+        WebElement myPlaylist = getDriver().findElement(By.xpath("//section[@id='playlists']//a[contains(text(), '" + nameOfPlaylist + "')]"));
         myPlaylist.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[class='del btn-delete-playlist']")));
     }
     public void deleteThePlaylist() {
-        WebElement playlistDeleteBtn = driver.findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
+        WebElement playlistDeleteBtn = getDriver().findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
         playlistDeleteBtn.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
     }
     public void clickHome() {
-        WebElement homeBtn = driver.findElement(By.xpath("//nav[@id='sidebar']//a[@class='home']"));
+        WebElement homeBtn = getDriver().findElement(By.xpath("//nav[@id='sidebar']//a[@class='home']"));
         homeBtn.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[class='home active']")));
     }
     public void clickRegistrationLink() {
 
-        WebElement registrationLink = driver.findElement(By.cssSelector("a[type='submit']"));
+        WebElement registrationLink = getDriver().findElement(By.cssSelector("a[type='submit']"));
         registrationLink.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[value='Register']")));
     }
     public void renameThePlaylistWithDoubleClick(String playlistName, String renamedPlaylist) {
-        WebElement myPlaylistName = driver.findElement(By.xpath("//section[@id='playlists']//a[contains(text(), '" + playlistName + "')]"));
+        WebElement myPlaylistName = getDriver().findElement(By.xpath("//section[@id='playlists']//a[contains(text(), '" + playlistName + "')]"));
         actions.doubleClick(myPlaylistName).perform();
         WebElement playlistNameInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
         playlistNameInputField.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.BACK_SPACE));
